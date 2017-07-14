b5fee82204780eac519d6923d12da657b01478fd
davidhope <flav.flavor@gmail.com>
2017-07-14 16:32:13 -0500

    initial vue setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <message>hello there</message>
+    <message>hello there2</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue'
+export default {
+  name: 'app',
+  components: { Message },
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="box">
+		<p>
+			<slot></slot>
+		</p>
+	</div>
+</template>
+<script>
+	export default{
+
+	}
+</script>
+
+<style>
+	.box {background:#999;border: 1px solid #000;}
+</style>
